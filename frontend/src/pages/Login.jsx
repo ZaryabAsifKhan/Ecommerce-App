095e24fda14d0186fc59e7afe9a6553fc22d9204
ZARYAB ASIF KHAN <zaryabasifkhan925@gmail.com>
2026-07-06 21:53:44 +0500

    4:50:37 completed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 const Login = () => {
-  const [currentState, setcurrentState] = useState('Sign Up');
+  const [currentState, setcurrentState] = useState('Login');
 
 
 
@@ -9,8 +9,12 @@ const Login = () => {
   return (
     <form className = 'flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
       <div className = 'inline-flex items-center gap-2 mb-2 mt-10'>
-
+        <p className='prata-regular text-3xl'>{currentState}</p>
+        <hr className = 'border-none h-[1.5px] w-8 bg-gray-800'/>
       </div>
+      {currentState !== 'Login' && <input type ='text' className='w-full px-3 py-2 border border-gray-800' placeholder ='Name' required />}
+      <input type ='email' className='w-full px-3 py-2 border border-gray-800' placeholder ='Email' required />
+      <input type ='password' className='w-full px-3 py-2 border border-gray-800' placeholder ='Password' required />
     </form>
   )
 }
